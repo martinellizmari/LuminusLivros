0a75fa3189d7e23dc53a789aa6398f4672d8924f
Mariana Amabile Martinelli <mariana.a.martinelli11@gmail.com>
2025-07-30 09:25:17 +0200

    Initial Commit

---
+++ LivrosLuminus/LivrosLuminusApp.swift
@@ -0,0 +1,17 @@
+//
+//  LivrosLuminusApp.swift
+//  LivrosLuminus
+//
+//  Created by Mariana Amábile Martinelli on 30/07/25.
+//
+
+import SwiftUI
+
+@main
+struct LivrosLuminusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
